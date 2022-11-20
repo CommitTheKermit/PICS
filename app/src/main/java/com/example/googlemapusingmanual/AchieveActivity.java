daf2f04232acb64795d7edebc67bdb3ba898a363
seogangsua <seogangsua99@gmail.com>
2022-11-20 16:59:44 +0900

    업적탭과 기타탭 기본틀 구현

---
+++ app/src/main/java/com/example/googlemapusingmanual/AchieveActivity.java
@@ -0,0 +1,134 @@
+package com.example.googlemapusingmanual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class AchieveActivity extends AppCompatActivity {
+    int intWater=0;
+    int intWalk=0;
+    int intRun=0;
+    int intBicycle=0;
+    int intTime=0;
+    String water[] = {
+            "물 10L 마시기",
+            "물 20L 마시기",
+            "물 30L 마시기",
+            "물 40L 마시기",
+            "물 50L 마시기"
+    };
+    String walk[] = {
+            "1km 걷기",
+            "10km 걷기",
+            "20km 걷기",
+            "30km 걷기",
+            "40km 걷기"
+    };
+    String run[] = {
+            "1km 달리기",
+            "10km 달리기",
+            "20km 달리기",
+            "30km 달리기",
+            "40km 달리기"
+    };
+    String bicycle[] = {
+            "1km 자전거 타기",
+            "10km 자전거 타기",
+            "20km 자전거 타기",
+            "30km 자전거 타기",
+            "40km 자전거 타기"
+    };
+    String time[] = {
+            "1시간 운동하기",
+            "10시간 운동하기",
+            "20시간 운동하기",
+            "30시간 운동하기",
+            "40시간 운동하기"
+    };
+
+    private int increase(int n){
+        n++;
+        if(n == 5){
+            n = 0;
+        }
+        return n;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achieve);
+
+        // 업적 체크박스
+        CheckBox[] checkBoxs = {
+                (CheckBox)findViewById(R.id.checkBox1),
+                (CheckBox)findViewById(R.id.checkBox2),
+                (CheckBox)findViewById(R.id.checkBox3),
+                (CheckBox)findViewById(R.id.checkBox4),
+                (CheckBox)findViewById(R.id.checkBox5)
+        };
+        checkBoxs[0].setText(water[0]);
+        checkBoxs[1].setText(walk[0]);
+        checkBoxs[2].setText(run[0]);
+        checkBoxs[3].setText(bicycle[0]);
+        checkBoxs[4].setText(time[0]);
+
+        checkBoxs[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxs[0].isChecked()) {
+                    Toast.makeText(getApplicationContext(), "완료", Toast.LENGTH_SHORT).show();
+                    intWater = increase(intWater);
+                    checkBoxs[0].setText(water[intWater]);
+                }
+            }
+        });
+
+        checkBoxs[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxs[1].isChecked()) {
+                    Toast.makeText(getApplicationContext(), "완료", Toast.LENGTH_SHORT).show();
+                    intWalk = increase(intWalk);
+                    checkBoxs[1].setText(walk[intWalk]);
+                }
+            }
+        });
+
+        checkBoxs[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxs[2].isChecked()) {
+                    Toast.makeText(getApplicationContext(), "완료", Toast.LENGTH_SHORT).show();
+                    intRun = increase(intRun);
+                    checkBoxs[2].setText(run[intRun]);
+                }
+            }
+        });
+
+        checkBoxs[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxs[3].isChecked()) {
+                    Toast.makeText(getApplicationContext(), "완료", Toast.LENGTH_SHORT).show();
+                    intBicycle = increase(intBicycle);
+                    checkBoxs[3].setText(bicycle[intBicycle]);
+                }
+            }
+        });
+
+        checkBoxs[4].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxs[4].isChecked()) {
+                    Toast.makeText(getApplicationContext(), "완료", Toast.LENGTH_SHORT).show();
+                    intTime = increase(intTime);
+                    checkBoxs[4].setText(time[intTime]);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/googlemapusingmanual/EtcTabActivity.java
@@ -0,0 +1,107 @@
+package com.example.googlemapusingmanual;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class EtcTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etc_tab);
+    }
+
+    public void onClickWithdrawalButton(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EtcTabActivity.this);
+        builder.setTitle("회원 탈퇴");
+        builder.setMessage("회원 탈퇴하시겠습니까?");
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                withdrawal();
+            }
+        });
+        builder.setNegativeButton("아니오", null);
+
+        builder.create().show();
+    }
+
+    public void onClickSetMainScreenButton(View v) {
+        setMainScreen();
+    }
+
+    public void onClickSetAlarmButton(View v) {
+        Intent intent = new Intent(EtcTabActivity.this, NotificationSettingActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    // etcLogic
+    public void setMainScreen() {
+        String[] mainScreenStrings = {"업적", "랭킹", "지도", "체중 기록", "기타"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EtcTabActivity.this);
+        builder.setTitle("메인 화면 설정");
+
+        builder.setItems(mainScreenStrings, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // TODO: 메인 화면 설정 로직이 추가되면 여기에 추가
+                switch (i) {
+                    case 0:
+                        // 업적
+                        break;
+                    default:
+                        // ...
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+
+    public void withdrawal() {
+        // TODO: 회원 탈퇴 기능 추가
+        /* if (회원탈퇴 됐으면) {
+
+        } */
+        AlertDialog.Builder builder = new AlertDialog.Builder(EtcTabActivity.this);
+        builder.setTitle("회원 탈퇴");
+        builder.setMessage("회원 탈퇴가 되었습니다.");
+        builder.setPositiveButton("확인", null);
+
+        builder.create().show();
+    }
+
+    public void onClickWeatherDelayButton(View v){
+        weatherDelaySetting();
+    }
+
+    public void weatherDelaySetting() {
+        String[] mainScreenStrings = {"1분", "5분", "10분"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(EtcTabActivity.this);
+        builder.setTitle("날씨 정보 딜레이 설정");
+
+        builder.setItems(mainScreenStrings, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // TODO: 날씨 정보 딜레이 설정 로직이 추가되면 여기에 추가
+                switch (i) {
+                    case 0:
+                        // 업적
+                        break;
+                    default:
+                        // ...
+                }
+            }
+        });
+
+        builder.create().show();
+    }
+}
