0e58c5dde39c6ea401d52f8aed98acf7a3f65c7d
양혁진 <86283893+injjan@users.noreply.github.com>
2022-11-29 14:59:03 +0900

    1.2
    1.2.1

--- app/src/main/java/com/example/googlemapusingmanual/weightTab.java
+++ app/src/main/java/com/example/googlemapusingmanual/weightTab.java
@@ -17,12 +17,23 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
@@ -39,25 +50,50 @@ public class weightTab extends Fragment {
     private ArrayList<TextView> textViewArrayList;
     private ImageView imageView1;
 
-    float height_cm = 173;
+    float height_cm = 182;
     float height_m = height_cm / 100;
-    float weight = 80;
+    float weight = 90;
     float ideal_weight = (height_cm - 100) * 0.9f;
     float bmi = weight / (height_m * height_m);
     float calculate = (bmi-15)*29.2f;
     String state = "초기값";
-
+    String line;
+    String[] rLine = new String[5];
 
     int rank = 0;
 
+    String readtext(String fName) {
+        String textStr = null;
+        FileInputStream inFs;
+        try {
+            inFs = openFileInput(fName);
 
-
-    float latest_weight;
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        if (bmi < 20) {
+
+        try{
+            // 데이터 파일 읽기 raw 폴더의 Rersonal.txt
+            BufferedReader bfRead = new BufferedReader(new InputStreamReader(getResources().openRawResource(Personal_Info)));
+            // 한줄씩 NULL이 아닐때까지 읽어 rLine 배열에 넣는다
+            int line_count = 0;
+            while ((line = bfRead.readLine()) != null){
+                rLine[line_count] = line;
+                line_count++;
+            }
+            bfRead.close();
+
+            Float.parseFloat(rLine[2]) = weight;
+
+        }catch(Exception e){
+            e.printStackTrace();;
+        }
+
+
+       if (bmi < 20) {
             String state = "저체중";
         }
         else if (bmi < 25) {
@@ -72,7 +108,6 @@ public class weightTab extends Fragment {
         else {
             state = "초고도비만";
         }
-
 
         View rootView = inflater.inflate(R.layout.fragment_weight_tab, container, false);
 
@@ -106,7 +141,6 @@ public class weightTab extends Fragment {
 
         return rootView;
 
-        // return inflater.inflate(R.layout.fragment_ranking_tab, container, false);
-    }
+        }
 
 }
